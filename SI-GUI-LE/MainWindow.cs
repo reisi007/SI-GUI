32a217d63d0592bd100571dbd4c1b477ac437c30
Florian Reisinger <florei@libreoffice.org>
2013-07-19 19:26:19 +0200

    First step towards Linux Edition [LE] Currently UI only!!!

---
+++ SI-GUI-LE/MainWindow.cs
@@ -0,0 +1,21 @@
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+	protected void start_dl_master (object sender, EventArgs e)
+	{
+
+	}
+
+}
